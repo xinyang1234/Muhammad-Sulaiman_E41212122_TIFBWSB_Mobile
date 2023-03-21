cabff312a9732506d6a2dc5b5d54adc9664c6c4c
Muhammad Sulaiman <91898723+xinyang1234@users.noreply.github.com>
2023-03-22 01:23:44 +0700

    Minggu7_Fragment

--- Minggu1-5/AplikasiPertama/mobile/src/main/java/com/example/aplikasipertama/Dashboard.java
+++ Minggu1-5/AplikasiPertama/mobile/src/main/java/com/example/aplikasipertama/Dashboard.java
@@ -1,19 +1,52 @@
 package com.example.aplikasipertama;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Dashboard extends AppCompatActivity {
     Button LinearLayout,RelativeLayout,ConstraintLayout1,TabelLayout,
     MaterialDesign,ScrollView,ScrollViewHorizontal,Location;
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    AccountFragment accountFragment = new AccountFragment();
+    LogoutFragment logoutFragment = new LogoutFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.r,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.r,homeFragment).commit();
+                        return true;
+                    case R.id.logout:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.r,logoutFragment).commit();
+                        return true;
+                    case R.id.account:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.r,accountFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
         LinearLayout = findViewById(R.id.btnLinearLayout);
         RelativeLayout = findViewById(R.id.btnRelativeLayout);
         ConstraintLayout1 = findViewById(R.id.btnConstraintLayout);
